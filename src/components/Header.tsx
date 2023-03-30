4219e25e00d26f7e02a967743b93041320793604
Matheus Delduque <matheusdelduque1@gmail.com>
2023-03-30 15:44:58 -0300

    Added window to create profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import { BiSearchAlt, BiUser } from "react-icons/bi";
 
-const Header = () => {
+interface Props {
+  setCreateProfile: () => void;
+}
+
+const Header = ({ setCreateProfile }: Props) => {
   return (
     <header className="w-full ">
       <div className="flex flex-row items-center">
@@ -9,14 +13,20 @@ const Header = () => {
           <Image src="/favicon.jpeg" alt="Logo" width={120} height={10} />
         </div>
         <div className="mr-20 flex w-1/2 flex-row-reverse">
-          <BiUser size={30} color="red" />
+          <button onClick={() => setCreateProfile()}>
+            <BiUser size={30} color="red" />
+          </button>
           <form className="mr-10 flex flex-row rounded-3xl bg-white">
             <input
               className="rounded-3xl indent-3"
-              type="search"
+              type="text"
               placeholder="Buscar"
             />
-            <BiSearchAlt size={30} color="red" className="absolute ml-[9.5rem]"/>
+            <BiSearchAlt
+              size={30}
+              color="red"
+              className="absolute ml-[9.5rem]"
+            />
           </form>
         </div>
       </div>
